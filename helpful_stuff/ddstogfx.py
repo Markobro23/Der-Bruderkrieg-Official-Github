97323a9fcf01d6fbf3e74378db841a4d4b4a20aa
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2022-08-06 22:19:57 -0400

    epic new westphalia icons

--- helpful_stuff/ddstogfx.py
+++ helpful_stuff/ddstogfx.py
@@ -1,57 +1,22 @@
-###DDS TO GFX, for HOI4 modding icons and other things like dual leaders, by ArjtheGreat
 import glob
 import os
 
-path = r"gfx\leaders\Generic"
-f = open(r"helpful_stuff\output.txt", "w")
-
+path = r"gfx\interface\goals"
 
 os.chdir(path)
-#print(os.listdir())
-files = glob.glob("*.png")
-#print(files)
+print(os.listdir())
+files = glob.glob("*.dds")
+print(files)
 
 files.sort(key=os.path.getmtime)
 
-
 for filename in files:
    filenamestorage = filename[:-4]
-   if filename.endswith(".png"):
-    
-    # CODE FOR GFX
-    
-    # f.write("spriteType = {\n")
-    # f.write(" " + "name =" + '"' + "GFX_" + filenamestorage + '"\n') 
-    # f.write(" " + "texturefile = " + '"' + "gfx/leaders/Generic/" + filename + '"\n')
-    # f.write("}\n")
-    # f.write("\n")
-
-    
-
-    # # CODE FOR SCRIPTED LOC
-    # if "navy" not in filename and "land" not in filename:
-    #     f.write("text = {\n")
-    #     f.write(" " + "trigger = {\n")
-    #     f.write(" " + " " + "has_country_flag =" + filenamestorage + "_flag\n") 
-    #     #f.write(" " + " " + "texturefile = " + '"' + "gfx/leaders/Generic/" + filename + '"')
-    #     f.write(" " + "}\n")
-    #     f.write(" " + "localization_key = GFX_" + filenamestorage + "\n")
-    #     f.write("}\n")
-    #     f.write("")
-    #     f.write("")
-    # else:
-    #     print("giannis")
-
-    # CODE FOR FLAGS
-    if "Asia" in filename:
-        f.write("1 = {\n")
-        f.write(" " + " " + "set_country_flag =" + filenamestorage + "_flag\n") 
-        #f.write(" " + " " + "texturefile = " + '"' + "gfx/leaders/Generic/" + filename + '"')
-        f.write(" " + "}\n")
-        f.write("")
-        f.write("")
-    else:
-        print("giannis")
+   if filename.endswith(".dds"):
+    print("spriteType = {")
+    print(" " + "name =" + '"' + "GFX_goal_" + filenamestorage + '"') 
+    print(" " + "texturefile = " + '"' + "gfx/interface/goals/" + filename + '"')
+    print("}")
+    print("")
    else:
        continue
-
